3daea5f0166885ec50731b6c8e663bc6bf1fd182
gnetboy <gnetboy.y@gmail.com>
2021-07-27 19:54:10 -0700

    commit tree fd0e1ac39decd2be0a4b6bcc792e909dbd813655

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array,birthday){
+ let thankyouMessages = []
+for (let i = 0; i < array.length; i++) {
+    thankyouMessages.push(`Thank you, ${array[i]}, for the wonderful ${birthday} gift!`);
+  }
+  return thankyouMessages
+}
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
